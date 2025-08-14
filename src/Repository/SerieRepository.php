15f40f830814f35852f314c335bb4f3986cf1d7f
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-08-14 16:59:29 +0200

    added route for catalog

--- src/Repository/SerieRepository.php
+++ src/Repository/SerieRepository.php
@@ -16,6 +16,50 @@ class SerieRepository extends ServiceEntityRepository
         parent::__construct($registry, Serie::class);
     }
 
+    public function findSeriesCustom(float $popularity, float $vote, int $nbMaxResults, int $offset): array
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.popularity > :popularity')
+            ->andWhere('s.vote > :vote')
+            ->orderBy('s.popularity', 'DESC')
+            ->addOrderBy('s.firstAirDate', 'DESC')
+            ->setParameter('popularity',$popularity)
+            ->setParameter('vote',$vote)
+            ->setMaxResults($nbMaxResults)
+            ->setFirstResult($offset)
+            ->getQuery()
+            ->getResult();
+    }
+
+
+        public function findSeriesWithDQL(float $popularity, float $vote, int $nbMaxResults, int $offset): array {
+            $dql = "SELECT s FROM App\Entity\Serie s WHERE (s.popularity > :popularity) OR s.vote > :vote
+                    ORDER BY s.firstAirDate DESC ";
+            return $this->getEntityManager()->createQuery($dql)
+                ->setMaxResults($nbMaxResults)
+                ->setFirstResult($offset)
+                ->setParameter('popularity',$popularity)
+                ->setParameter('vote',$vote)
+                ->execute();
+
+        }
+
+        public function getSeriesWithRawSQL(float $popularity, float $vote): array
+        {
+            $sql= <<<SQL
+            SELECT * FROM serie s 
+                WHERE (s.popularity > :popularity OR s.first_air_date > :date)
+                AND s.vote > :vote
+            ORDER BY s.first_air_date DESC, s.popularity DESC
+            LIMIT 10 OFFSET 0            
+            SQL;
+
+
+            $conn = $this->getEntityManager()->getConnection();
+            return $conn->prepare($sql)->executeQuery(['popularity'=>$popularity,'date'=> (new \DateTime('- 5 years'))->format('Y-m-d'),'vote'=>$vote])
+                ->fetchAllAssociative();
+
+        }
 //    /**
 //     * @return Serie[] Returns an array of Serie objects
 //     */
